cd3f44edf1b10ea0130c74d6c8bc7f7a30a44abb
T-feng <1263032850@qq.com>
2023-04-26 18:05:10 +0800

    调整滚动条

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -4,7 +4,7 @@
 
     // custom scrollbar
 
-    $("html").niceScroll({styler:"fb",cursorcolor:"#65cea7", cursorwidth: '6', cursorborderradius: '0px', background: '#424f63', spacebarenabled:false, cursorborder: '0',  zindex: '1000'});
+    $("html").niceScroll({styler:"fb",cursorcolor:"#65cea7", cursorwidth: '16', cursorborderradius: '0px', background: '#424f63', spacebarenabled:false, cursorborder: '0',  zindex: '1000'});
 
     $(".left-side").niceScroll({styler:"fb",cursorcolor:"#65cea7", cursorwidth: '3', cursorborderradius: '0px', background: '#424f63', spacebarenabled:false, cursorborder: '0'});
 

37b51657ead60a85adc6bf5afdaee6f82a9dde4b
惊鸿过隙 <taig.sun@outlook.com>
2023-04-27 10:40:10 +0800

    ‘’

---
+++ src/main/java/com/example/amm/Test.java
@@ -0,0 +1,22 @@
+package com.example.amm;
+
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.LocalDateTimeUtil;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String dateStr = "2023-04-16T09:23:56";
+        DateTime dt = DateUtil.parse(dateStr);
+
+        // Date对象转换为LocalDateTime
+        LocalDateTime time = LocalDateTimeUtil.of(dt);
+        Duration between = LocalDateTimeUtil.between(time, LocalDateTimeUtil.now());
+        System.out.println(24*3600);
+        System.out.println(between.getSeconds());
+    }
+}

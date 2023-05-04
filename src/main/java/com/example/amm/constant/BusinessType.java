5bf21fc654637369182ede126126a80271dda117
惊鸿过隙 <taig.sun@outlook.com>
2023-05-04 10:04:10 +0800

    ‘’

--- src/main/java/com/example/amm/constant/BusinessType.java
+++ src/main/java/com/example/amm/constant/BusinessType.java
@@ -1,12 +1,6 @@
 package com.example.amm.constant;
 
 public enum BusinessType {
-    ACCOUNT,
-    GROUP,
-    TASK,
-    AUTO_TASK,
-    AUTO_USER,
-    AUTO_ACCOUNT,
-    AUTO_GROUP;
+    ACCOUNT, GROUP, TASK, AUTO_TASK, AUTO_USER, AUTO_ACCOUNT, AUTO_GROUP
 
 }
--- src/main/java/com/example/amm/constant/RedisKeyConstant.java
+++ src/main/java/com/example/amm/constant/RedisKeyConstant.java
@@ -2,14 +2,12 @@ package com.example.amm.constant;
 
 public class RedisKeyConstant {
 
-
     public static String ACCOUNT_LOG_KEY = "amm:account:log:";
 
     public static String GROUP_LOG_KEY = "amm:group:log:";
 
     public static String TASK_LOG_KEY = "amm:task:log:";
-    
-    public static String AUTO_TASK_LOG_KEY = "amm:autoTask:log:";
 
+    public static String AUTO_TASK_LOG_KEY = "amm:autoTask:log:";
 
 }
